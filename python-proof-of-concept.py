7a0aaf2bc3876a84cb068b29c3030979d0ce1d18
BreslinIT <pokemax209@gmail.com>
2023-02-23 23:17:12 -0800

    python server testing

---
+++ python-proof-of-concept.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import time
+import math
+
+data = {
+    'id': '000000',
+    'timestamp': '',
+    'bathroom-id': ''
+}
+
+data['timestamp'] = math.floor(time.time())
+data['bathroom-id'] = data['timestamp'] % 5
+
+print(f"Sending data: {data}")
+
+r = requests.post('http://localhost:8000', data=data)
